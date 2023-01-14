6abd8ca890828460e733a671643538b63ecfdd40
andre-salvador <andrersalvador@hotmail.com>
2023-01-13 23:20:11 -0300

    automação de login

---
+++ automacao.py
@@ -0,0 +1,34 @@
+from os import system
+from time import sleep
+
+import pyautogui
+
+# variaveis
+site = 'https://www.proeis.rj.gov.br/Default.aspx'
+id = ''
+senha = ''
+
+# abrir navegador e entrar no site
+system('start chrome.exe')
+pyautogui.click(278, 52, duration=0.8)
+pyautogui.write(site)
+pyautogui.press('enter')
+
+# login
+pyautogui.click(436, 386, duration=1)
+pyautogui.click(425, 462, duration=0.2)
+
+pyautogui.click(388, 432, duration=0.9)
+pyautogui.write(id)
+pyautogui.click(389, 503, duration=0.3)
+pyautogui.write(senha)
+pyautogui.click(433, 733, duration=1)
+sleep(5)
+pyautogui.click(378, 815)
+pyautogui.click(429, 492, duration=0.8)
+pyautogui.click(382, 434, duration=0.2)
+pyautogui.click(420, 542, duration=0.4)
+pyautogui.press('m')
+pyautogui.press('m')
+pyautogui.press('m')
+pyautogui.press('enter')
